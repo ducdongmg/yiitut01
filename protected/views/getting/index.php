523bdce8621cff4902d11ab2cc6ed9d974736754
Dong Nguyen <ducdong.mg@gmail.com>
2014-05-12 23:07:25 +0700

    Hello world

---
+++ protected/views/getting/index.php
@@ -0,0 +1,13 @@
+<?php
+/* @var $this GettingController */
+
+$this->breadcrumbs=array(
+	'Getting',
+);
+?>
+<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
+
+<p>
+	<tt><?php echo $content; ?></tt>
+	<tt><?php echo $this->mess; ?></tt>
+</p>
