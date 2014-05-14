3b472c12f56f2a2b7648322da08c4866742668d1
ducdongmg <ducdong.mg@gmail.com>
2014-05-14 19:08:09 +0700

    generate states model/controller

---
+++ protected/views/states/_search.php
@@ -0,0 +1,35 @@
+<?php
+/* @var $this StatesController */
+/* @var $model States */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'state'); ?>
+		<?php echo $form->textField($model,'state',array('size'=>25,'maxlength'=>25)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'population'); ?>
+		<?php echo $form->textField($model,'population'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
